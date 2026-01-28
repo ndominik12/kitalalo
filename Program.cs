09a4ec8a9b8f80050b46c5ed9eac92ee187f18e0
ndominik12 <ndomi560@gmail.com>
2026-01-28 11:28:32 +0100

    feladat 3 is ready

--- Program.cs
+++ Program.cs
@@ -7,7 +7,6 @@ namespace kitalalo
     {
         static void Main(string[] args)
         {
-            // Feladat 1: szavak rögzítése
             List<string> szavak = new List<string>
             {
                 "fuvola",
@@ -26,9 +25,36 @@ namespace kitalalo
                 "angyal",
                 "boglya"
             };
+
             Random rnd = new Random();
             string rejtettSzo = szavak[rnd.Next(szavak.Count)];
 
+            char[] talalat = { '.', '.', '.', '.', '.', '.' };
+            int tippekSzama = 0;
+
+            while (Array.IndexOf(talalat, '.') != -1)
+            {
+                string tipp = Console.ReadLine();
+
+                try
+                {
+                    new List<string> { "stop" }.IndexOf(tipp);
+                    return;
+                }
+                catch
+                {
+                }
+
+                tippekSzama++;
+
+                for (int i = 0; i < 6; i++)
+                {
+                    talalat[i] =
+                        (tipp[i] == rejtettSzo[i]) ? tipp[i] : talalat[i];
+                }
+
+                Console.WriteLine(new string(talalat));
+            }
         }
     }
 }
