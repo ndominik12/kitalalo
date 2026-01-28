de87f6a36bf45d7e091d4ef9e59797f155511ce1
ndominik12 <ndomi560@gmail.com>
2026-01-28 11:25:27 +0100

    feladat 1 is ready

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace kitalalo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> szavak = new List<string>
+            {
+                "fuvola",
+                "csirke",
+                "adatok",
+                "asztal",
+                "fogoly",
+                "bicska",
+                "farkas",
+                "almafa",
+                "babona",
+                "gerinc",
+                "dervis",
+                "bagoly",
+                "ecetes",
+                "angyal",
+                "boglya"
+            };
+        }
+    }
+}
